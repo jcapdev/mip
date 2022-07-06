d5e85b395eac7110de2b188ed2a8a56382c0a582
jcapdev <aguila08ruso2@gmail.com>
2022-07-06 10:59:10 -0500

    imagenes de clientes

---
+++ politicas.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+    <?php 
+       require_once "assets/php/head.php";
+       require_once "assets/php/nav.php";
+       require_once "assets/php/load.php";
+    ?>   
+
+    <body>
+		<style>
+			img {
+				max-width: 100%;
+			}
+		</style>
+
+        <!-- html boddy  -->
+        <!-- Sidebar Modal -->
+        <div class="sidebar-modal">
+            <div class="sidebar-modal-inner">
+                <div class="sidebar-about-area">
+                    <div class="title">
+                        <h2>About Us</h2>
+                        <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication. No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
+                    </div>
+                </div>
+
+                <div class="sidebar-instagram-feed">
+                    <h2>Blog</h2>
+
+                    <ul>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/1.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/2.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/3.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/4.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/5.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/6.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/7.jpg" alt="image">
+							</a>
+						</li>
+                        <li>
+							<a href="blog-details.html">
+								<img src="assets/img/blog/8.jpg" alt="image">
+							</a>
+						</li>
+                    </ul>
+                </div>
+
+                <div class="sidebar-contact-area">
+                    <div class="contact-info">
+                        <div class="contact-info-content">
+                            <h2>
+                                <a href="tel:+61-821-456">+61-821-456</a>
+                                <span>OR</span>
+                                <a href="mailto:hello@trice.com">hello@trice.com</a>
+                            </h2>
+    
+                            <ul class="social">
+                                <li>
+									<a href="#" target="_blank">
+										<i class="bx bxl-facebook"></i>
+									</a>
+								</li>
+                                <li>
+									<a href="#" target="_blank">
+										<i class="bx bxl-twitter"></i>
+									</a>
+								</li>
+                                <li>
+									<a href="#" target="_blank">
+										<i class="bx bxl-linkedin"></i>
+									</a>
+								</li>
+                                <li>
+									<a href="#" target="_blank">
+										<i class="bx bxl-youtube"></i>
+									</a>
+								</li>
+                                <li>
+									<a href="#" target="_blank">
+										<i class="bx bxl-instagram"></i>
+									</a>
+								</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <span class="close-btn sidebar-modal-close-btn">
+					<i class="bx bx-x"></i>
+				</span>
+            </div>
+        </div>
+		<!-- End Sidebar Modal -->
+		<!-- Start Page Title Area -->
+		<div class="page-title-area bg-1">
+			<div class="container">
+				<div class="page-title-content">
+					<h2>AVISO DE PRIVACIDAD</h2>
+					<p id = "textslider" style="direction: initial;">Manejo Integrado de Plagas de Oriente SA de CV.</p>	
+				</div>
+			</div>
+		</div>
+		<!-- End Page Title Area -->
+		<!-- Start Terms & Conditions Area -->
+		<section class="terms-conditions ptb-100">
+			<div class="container">
+				<div class="about-content" style="direction: ltr;text-align: center;">
+					<h2>Welcome To Trice Privacy Policy</h2>
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+					<h2>Welcome To Help Trice Privacy</h2>
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+					<h2>Welcome To Services Trice Policy</h2>
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+					<p class = "textabout">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+				</div>
+			</div>
+		</section>
+		<!-- End Terms & Conditions Area -->	
+	
+		<!-- Start Go Top Area -->
+		<div class="go-top">
+			<i class='bx bx-chevrons-up'></i>
+			<i class='bx bx-chevrons-up'></i>
+		</div>
+		<!-- End Go Top Area -->
+		<?php
+        require_once "assets/php/footer.php";
+        require_once "assets/php/script.php";
+        ?>    
+
+       
+    </body>
+</html>
